54072bd769e3f1ab8b20b3c78915cce9f23f1473
Julie <julie.trucvallet@gmail.com>
2025-08-26 21:42:52 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,6 @@
-import react from "@vitejs/plugin-react";
-import { copyFileSync } from "fs";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-function copyRedirects() {
-  return {
-    name: "copy-redirects",
-    closeBundle() {
-      try {
-        copyFileSync("public/_redirects", "dist/_redirects");
-        console.log("✅ Fichier _redirects copié dans dist/");
-      } catch (err) {
-        console.error("⚠️ Impossible de copier _redirects :", err);
-      }
-    },
-  };
-}
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), copyRedirects()],
-});
+  plugins: [react()],
+})
